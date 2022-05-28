255391c836eb0c87fbba4f302a4f1a06b9abb045
Huf <bjhufstetler@gmail.com>
2022-05-28 09:01:19 -0700

    build: starts menubar build

--- ui/src/components/Bar.jsx
+++ ui/src/components/Bar.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { AppBar, Container, IconButton, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useFetch } from '../hooks';
+import { useAppContext } from '../context';
+import { Box } from '@mui/system';
+import { useEffect } from 'react';
 
 export const Bar = () => {
+    const appContext = useAppContext();
+    const { data: users } = useFetch(appContext.userURL)
+    useEffect(() => {
+        console.log(users)
+    }, [users])
     return (
-        <>Menu Bar</>
+        <AppBar position='static'>
+            <Container maxWidth='xl'>
+                <Toolbar disableGutters sx={{display: 'flex', justifyContent: 'space-around'}}>
+                    <Box sx={{flexGrow:0}}>
+                        <IconButton>
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
