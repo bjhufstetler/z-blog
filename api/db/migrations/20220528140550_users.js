146112ba3ed3f9470e2488fcfb656c20c0c6d023
Huf <bjhufstetler@gmail.com>
2022-05-28 07:10:58 -0700

    build: creates database migrations, seeds, api endpoints

---
+++ api/db/migrations/20220528140550_users.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema
+      .createTable('users', table => {
+          table.increments('id');
+          table.string('first', 255);
+          table.string('last', 255);
+          table.string('email', 255);
+          table.string('username', 255);
+          table.string('password', 255);
+          table.string('salt', 255);
+      });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('users');
+  };
--- api/src/app.js
+++ api/src/app.js
@@ -23,5 +23,68 @@ app.get('/authors', (request, response) => {
 
 })
 
+app.get('/api', (req, res) => {
+    res.status(200).send('It works!')
+})
+
+app.get('/api/:table', (req, res) => {
+    let base = knex(req.params.table)
+    if(req.query.id) base = base.where('id', req.query.id)
+    if(req.query.user_id) base = base.where('user_id', req.query.user_id)
+    base
+    .then(data => res.status(200).json(data))
+    .catch(err => {throw Error(err)});
+})
+
+app.post('/api/:table', (req, res) => {
+    knex(req.params.table)
+    .insert(req.body)
+    .then(data => res.status(201).json(data))
+    .catch(err => {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'An error occured, please contact your network administrator.',
+            error: err
+        });
+    });
+});
+
+app.patch('/api/:table/:id', (req, res) => {
+    knex(req.params.table)
+    .where('id', req.params.id)
+    .update(req.body)
+    .then(() => {
+        knex(req.params.table)
+        .then(data => res.status(200).json(data));
+    })
+    .catch(err => {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'An error occurred, please contact your network administrator.',
+            error: err
+        });
+    });
+});
+
+app.delete('/api/:table/:id',(req, res) => {
+    knex(req.params.table)
+    .where('id', req.params.id)
+    .del()
+    .then(() => {
+        knex(req.params.table)
+        .then(data => res.status(200).json(data));
+    })
+    .catch(err => {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'An error occurred, please contact your network administrator.',
+            error: err
+        });
+    });
+});
+
 module.exports = app;
 
