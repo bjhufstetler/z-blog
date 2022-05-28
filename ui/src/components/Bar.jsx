0f197b22d195c95d20653aab1dedeef5f95124fe
Huf <bjhufstetler@gmail.com>
2022-05-28 09:29:34 -0700

    refactor: replaces useFetch with fetch

--- ui/src/components/Bar.jsx
+++ ui/src/components/Bar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { AppBar, Container, IconButton, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useFetch } from '../hooks';
-//import { useAppContext } from '../context';
 import { Box } from '@mui/system';
-import { useEffect } from 'react';
+import config from './config'
+const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 export const Bar = () => {
-    //const appContext = useAppContext();
-    const { data: users } = useFetch('users')
-    useEffect(() => {
-        console.log(users)
-    }, [users])
+    const tmpUsers = [
+        {id: 1, first: 'Aaron', last: 'Aardvark', username: 'aaa', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'},
+        {id: 2, first: 'Billy', last: 'Blue', username: 'bluebilly', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'},
+        {id: 3, first: 'Carl', last: 'Carlson', username: 'carl2carl', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'}
+      ]
+    const [users, setUsers] = useState(tmpUsers)
+    fetch(`${ApiUrl}/api/users`)
+      .then(res => setUsers(res))
+
     return (
         <AppBar position='static'>
             <Container maxWidth='xl'>
@@ -20,6 +23,9 @@ export const Bar = () => {
                         <IconButton>
                             <MenuIcon />
                         </IconButton>
+                        {users.map((user, index) => {
+                            return(<Typography key={index}>{user.first}</Typography>)
+                        })}
                     </Box>
                 </Toolbar>
             </Container>
--- ui/src/config.js
+++ ui/src/config.js
@@ -5,7 +5,7 @@ const config = {
     },
 
     production: {
-        apiUrl: "https://bsdi-poc-api.herokuapp.com"
+        apiUrl: "https://z-blog-api.herokuapp.com"
     },
     test: {
         apiUrl: ''
