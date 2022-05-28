15ac8d49372ac945d6452410a975d6752e6adee1
Huf <bjhufstetler@gmail.com>
2022-05-28 10:27:33 -0700

    refactor: removes all hooks

--- ui/src/components/Bar.jsx
+++ ui/src/components/Bar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AppBar, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box } from '@mui/system';
@@ -7,12 +7,12 @@ import { Box } from '@mui/system';
 //const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 export const Bar = () => {
-    const tmpUsers = [
+    const users = [
         {id: 1, first: 'A', last: 'Aardvark', username: 'aaa', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'},
         {id: 2, first: 'B', last: 'Blue', username: 'bluebilly', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'},
         {id: 3, first: 'C', last: 'Carlson', username: 'carl2carl', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'}
       ]
-    const [users, setUsers] = useState(tmpUsers)
+    //const [users, setUsers] = useState(tmpUsers)
     
     
     /*
