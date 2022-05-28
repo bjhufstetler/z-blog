b27468dfaa5ec77f739c488822b9e11935e7b729
Huf <bjhufstetler@gmail.com>
2022-05-28 10:40:31 -0700

    refactor: now nothing works, everything is removed but memory leak persists

--- ui/src/components/Bar.jsx
+++ ui/src/components/Bar.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { AppBar, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box } from '@mui/system';
-//import config from '../config'
-//import { useEffect } from 'react';
-//const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 export const Bar = () => {
     const users = [
@@ -12,22 +9,6 @@ export const Bar = () => {
         {id: 2, first: 'B', last: 'Blue', username: 'bluebilly', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'},
         {id: 3, first: 'C', last: 'Carlson', username: 'carl2carl', password: '$2a$10$nuVbHXPE7zCqUp5cyK25AO2g2Uc6KPFkHMjHHhrAaf6Z6W2829mFa', salt: '$2a$10$nuVbHXPE7zCqUp5cyK25AO'}
       ]
-    //const [users, setUsers] = useState(tmpUsers)
-    
-    
-    /*
-    useEffect(() => {
-        setUsers(users)
-    }, []);
-    const getUsers = () => {
-        fetch(`${ApiUrl}/api/users`)
-            .then(res => {
-                console.log(res)
-                setUsers(res)
-            })
-
-    };
-    */
 
     return (
         <AppBar position='static'>
@@ -37,12 +18,9 @@ export const Bar = () => {
                         <IconButton>
                             <MenuIcon />
                         </IconButton>
-                        {users ? 
-                            users.map((user, index) => {
+                        {users.map((user, index) => {
                                 return(<Typography key={index}>{user.first}</Typography>)
-                            })
-                            : null
-                        }
+                            })}
                     </Box>
                 </Toolbar>
             </Container>
