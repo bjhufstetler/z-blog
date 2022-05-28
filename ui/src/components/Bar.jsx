2bbea99ee6ac72a8a29310fb90c4b2974f84c8fb
Huf <bjhufstetler@gmail.com>
2022-05-28 09:07:22 -0700

    refactor: the problem is in useEffect, useFetch, or appContext

--- ui/src/components/Bar.jsx
+++ ui/src/components/Bar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { AppBar, Container, IconButton, Toolbar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useFetch } from '../hooks';
+//import { useFetch } from '../hooks';
 import { useAppContext } from '../context';
 import { Box } from '@mui/system';
 import { useEffect } from 'react';
 
 export const Bar = () => {
     const appContext = useAppContext();
-    const { data: users } = useFetch(appContext.userURL)
+    //const { data: users } = useFetch(appContext.userURL)
     useEffect(() => {
-        console.log(users)
-    }, [users])
+        console.log(appContext)
+    }, [appContext])
     return (
         <AppBar position='static'>
             <Container maxWidth='xl'>
