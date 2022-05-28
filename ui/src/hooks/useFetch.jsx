82b125a7ffdd1794f98f0dcd6523b872301d8fe8
Huf <bjhufstetler@gmail.com>
2022-05-28 07:22:02 -0700

    build: adds useFetch hook

---
+++ ui/src/hooks/useFetch.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import config from '../config';
+const ApiUrl = config[process.env.REACT_APP_NODE_ENV || 'development'].apiUrl;
+
+export const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [err, setErr] = useState(null);
+    const [load, setLoad] = useState(false);
+    
+    useEffect(() => {
+        const baseURL = `${ApiUrl}/api/`
+        fetch(`${baseURL}${url}`)
+        .then( res => {
+            if(res.ok) {
+                console.log(res)
+                return res.json();
+            } else {
+                throw new Error('Cannot convert response to json');
+            };
+        })
+        .then( json => setData(json) )
+        .catch( e => setErr(e) )
+        .finally(() => setLoad(true));
+    }, [url]);
+
+    return { data, err, load };
+};
+
+useFetch.propTypes = {
+    url: PropTypes.string
+}
