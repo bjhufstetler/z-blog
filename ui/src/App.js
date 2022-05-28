025c9d5d19e2a41a9f8a11ce3e310379eb314c07
Huf <bjhufstetler@gmail.com>
2022-05-28 08:02:37 -0700

    chore: adds index files for context and hooks

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import config from './config'
-import { useAppContext } from './context/context';
-import { useFetch } from './hooks/useFetch';
+import { useAppContext } from './context';
+import { useFetch } from './hooks';
 //const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
---
+++ ui/src/context/index.js
@@ -0,0 +1,1 @@
+export { ContextProvider, useAppContext, useUpdateAppContext } from './context';
